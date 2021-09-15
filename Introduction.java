cfa3679ac9d99a5917f7fea3fe345ec00aa6b7bc
CooperWang0912 <90701697+CooperWang0912@users.noreply.github.com>
2021-09-14 20:01:32 -0400

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Cooper");
+        System.out.println("I am a Third Former in St. Mark's School");
+        System.out.println("I am from China, Shanghai");
+        System.out.println("I have being using C++ to program for about two years now");
+        System.out.println("I love programming");
+    }
+}
