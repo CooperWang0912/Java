016ddeaccf53bf52ebe5be62ed929942696f7531
CooperWang0912 <90701697+CooperWang0912@users.noreply.github.com>
2022-02-23 13:16:19 -0500

    Pokemon

--- Pokemon/src/pokemon/Dialga.java
+++ Pokemon/src/pokemon/Dialga.java
@@ -3,6 +3,6 @@ package pokemon;
 public class Dialga extends Pokemon{
 
     public Dialga(){
-        super("Dialga", 1, 150,"Dialga", new Move[]{new Move("Flamethrower", 1, 25, 1), new Move("Fireball", 1, 25, 0), new Move("Bombs", 0, 40, 4), new Move("Bomb", 0, 40, 0)});
+        super("Dialga", 1, 150,"Dialga.png", new Move[]{new Move("Flamethrower", 1, 25, 1), new Move("Fireball", 1, 25, 0), new Move("Bombs", 0, 40, 4), new Move("Bomb", 0, 40, 0)});
     }
 }
--- Pokemon/src/pokemon/Nuke.java
+++ Pokemon/src/pokemon/Nuke.java
@@ -3,6 +3,6 @@ package pokemon;
 public class Nuke extends Pokemon{
 
     public Nuke(){
-        super("Nuke", 1, 10,"Nuke", new Move[]{new Move("You got nuked", 0, 150, 1), new Move("", 0, 0, 0), new Move("", 0, 0, 0), new Move("", 0, 0, 0)});
+        super("Nuke", 1, 10,"Nuke.png", new Move[]{new Move("You got nuked", 0, 75, 1), new Move("You got bombed", 0, 35, 0), new Move("You got wrecked", 0, 40, 0), new Move("", 0, 0, 0)});
     }
 }
