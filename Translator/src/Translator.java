6189a652244dd1df9104ba32091ca90dd0f7ad3d
CooperWang0912 <90701697+CooperWang0912@users.noreply.github.com>
2022-04-26 19:31:59 -0400

    Translator

---
+++ Translator/src/Translator.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+public class Translator {
+
+    public static void main(String[]args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        Translator runner = new Translator();
+
+        HashMap<String, String> dictionary = new HashMap<String, String>();
+        BufferedReader in  = new BufferedReader(new FileReader("EnglishToArabicDictionary.txt"));
+
+        String FirstLine = in.readLine();
+
+        String SecondLine = in.readLine();
+
+        dictionary.put(FirstLine.substring(1), SecondLine);
+
+        for (String line = in.readLine(); line != null; line = in.readLine()){
+            dictionary.put(line, in.readLine());
+        }
+
+        in.close();
+
+        while (true) {
+            String a = scanner.nextLine();
+            System.out.println(dictionary.get(a));
+        }
+    }
+}
