62ae1859d092fcd9794e69469021438697caacec
CooperWang0912 <90701697+CooperWang0912@users.noreply.github.com>
2023-01-05 08:59:54 -0500

    Code Along

---
+++ HashMapCodeAlong/src/CodeAlong.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class CodeAlong {
+
+    public static void main(String[]args){
+        //Create the HashMap here
+
+        //Use the put function to put in numbers and names of your classmates
+
+        //Use the get function to get the names of your classmates from the HashMap
+
+        //Use the key function to get a list of the numbers
+
+        //Use the values function to get list of the names
+
+        //Play around with it and see what else you can do
+    }
+}
